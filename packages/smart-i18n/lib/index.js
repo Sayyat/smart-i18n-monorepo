7b0fded8b67f061c9faa4f93a295030901219c8a
Sayat <sayatraykulov@gmail.com>
2025-11-09 10:00:06 +0500

    fix: update smart-i18n/lib/index.js

--- packages/smart-i18n/lib/index.js
+++ packages/smart-i18n/lib/index.js
@@ -1,6 +1,8 @@
-export * from "./copy.js";
 export * from "./config.js";
+export * from "./copy.js";
+export * from "./namespaces.js";
 export * from "./paths.js";
+export * from "./type.js";
 
 // For now, smart-i18n-react uses only these apis.
 // There will be more exports as there are new needs
