063c2eaed7e0a24212c636e02b56a448d22f039f
Sayat <sayatraykulov@gmail.com>
2025-11-09 10:03:35 +0500

    feat: add next-i18n app

---
+++ apps/next-i18n/src/i18n/types/i18n.ts
@@ -0,0 +1,9 @@
+
+import type { TNamespace, TNamespaceTranslationKeys } from "../generated";
+
+export type TFunction<N extends TNamespace> = <
+  K extends TNamespaceTranslationKeys[N],
+>(
+  key: K,
+  options?: Record<string, unknown>,
+) => string;
