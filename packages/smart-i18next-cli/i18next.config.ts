af2c5b476abf0124fac3b4b23b22537aed609c23
Sayat <sayatraykulov@gmail.com>
2025-11-21 02:58:21 +0500

    refactor: update i18next.config.ts

--- packages/smart-i18next-cli/i18next.config.ts
+++ packages/smart-i18next-cli/i18next.config.ts
@@ -8,22 +8,18 @@ export default defineConfig({
 		"en",
 	],
 	extract: {
-		input: "src/**/*.{js,jsx,ts,tsx}",
+		input: ["src/**/*.{js,jsx,ts,tsx,vue,svelte}"],
 		ignore: "src/i18n/**/*.{js,jsx,ts,tsx}",
 		output: "src/i18n/locales/{{language}}/{{namespace}}.json",
 		primaryLanguage: "en",
 		nsSeparator: false,
 		keySeparator: false,
 		removeUnusedKeys: true,
+		sort: true,
 		defaultValue: (key,namespace, language, value ) => {
 			console.log({key,namespace, language, value})
 			return key;
 		},
-		sort: true,
-	},
-	types: {
-		input: "src/i18n/locales/{{language}}/{{namespace}}.json",
-		output: "src/i18n/types/{{language}}/{{namespace}}.ts"
 	},
 	plugins: [
 		SmartI18nPlugin()
